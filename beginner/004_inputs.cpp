e4885497acdcaf7dc26e6f3d80fdf589f73fcadb
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2020-12-09 19:38:00 +0530

    Feat: Taking user input from console

---
+++ beginner/004_inputs.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int numberOfRobots;
+
+    std::cout << "How many robots to activate?: " << std::endl;
+
+    std::cin >> numberOfRobots;
+
+    std::cout << numberOfRobots << " Robots Activated!" << std::endl;
+
+    return 0;
+}
