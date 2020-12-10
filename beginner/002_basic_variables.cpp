21493bbe3a4da649cfd911ca033398dd29490e0b
Eshan Mewantha Herath <eshanMewantha@users.noreply.github.com>
2020-12-10 14:52:07 +0530

    Feat: Update with double

--- beginner/002_basic_variables.cpp
+++ beginner/002_basic_variables.cpp
@@ -7,7 +7,9 @@ int main()
 
     int robot_wheels = 4; // integer
 
-    float height_to_camera = 1.232;  // float
+    float height_to_camera = 1.232;  // float | Size: 4 bytes | Precision: 7 decimal digits precision
+
+    double range_finder_measurement = 1.232732; // double | Size: 8 bytes | Precision: In general, 15 decimal digits precision
 
     std::string robot_name = "Mesee"; // string
 
